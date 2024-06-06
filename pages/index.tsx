6a40cff682767f1f254c0c5c02567e3168ef2621
caesar <454451758@qq.com>
2024-06-06 09:18:42 +0800

    chore: next comile with banner

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,7 @@
 import Head from "next/head";
-import { Inter } from "next/font/google";
 import { Button } from "antd";
 
 import styles from "@/styles/Home.module.css";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -15,7 +12,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${styles.main} ${inter.className}`}>
+      <main className={`${styles.main}`}>
         <Button type="primary">Antd Button</Button>
       </main>
     </>
